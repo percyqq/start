76fd36973a0bb595cd04d067f5acef133fff913b
Qing Qing <qingqing.qq@alibaba-inc.com>
2020-12-25 18:47:14 +0800

    1

--- src/main/java/org/learn/config/RedisProperties.java
+++ src/main/java/org/learn/config/RedisProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "spring.redis")
+//@ConfigurationProperties(prefix = "spring.redis")
 @Data
 public class RedisProperties {
     private int database = 0;
