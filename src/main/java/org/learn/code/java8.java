7cf85e0a3391893291837210865816f8911869e2
Qing Qing <qingqing.qq@alibaba-inc.com>
2021-01-07 19:07:23 +0800

    1

---
+++ src/main/java/org/learn/code/java8.java
@@ -0,0 +1,10 @@
+package org.learn.code;java8
+
+Function<String, Long> parseLong = str -> Long.parseLong(str);
+Map<Long, PartnerDishMapping> partnerDishMappingMap = dishMappings.stream().collect(
+        Collectors.toMap(item -> parseLong.apply(item.getSkuId()), Function.identity(), (oldValue, newValue) -> newValue));
+
+List<AuthUser> userList;
+Set<Long> alreadyAddedUserSet;
+//userList.stream().filter(t -> t != null).filter();
+Set<Long> userIdSet = userList.stream().filter(u -> !alreadyAddedUserSet.contains(u.getId())).map(AuthUser::getId).collect(Collectors.toSet());
