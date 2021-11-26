b03b5002c72dcd7508824502507b787e8a1ceb37
qingqing005 <qingqing005@BKJK.com>
2021-11-26 15:42:21 +0800

    binlog module

--- starter/src/main/java/org/learn/binlogconnector/代码优化点评.java
+++ starter/src/main/java/org/learn/binlogconnector/代码优化点评.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class 代码优化点评 {
 
@@ -20,17 +21,30 @@ public class 代码优化点评 {
             // 在另外一个类中使用了回调来使用当前类的变量。
             System.out.println(todo);
         });
-    }
 
+        System.out.println("-----");
+        wtf2(inputToUse, dataMadeByFunction -> {
+            String todo = dataMadeByFunction + "碉堡了";
+            // 在另外一个类中使用了回调来使用当前类的变量。
+            // 并且回传给当前函数。 相当于函数嵌套调用，但是 嵌套调用的函数内部使用了 外部函数的变量，而没有通过传递，
+            System.out.println(todo);
+            return todo;
+        });
+    }
 
     private static void wtf(String in, Consumer<String> action) {
         System.out.println("step 1");
-
         String input = in.toLowerCase(Locale.ROOT);
         action.accept(input);
-
         System.out.println("step 3");
     }
+
+    private static void wtf2(String input, Function<String, String> action) {
+        System.out.println("step 1");
+        String ret2 = action.apply(input);
+        System.out.println("step 3, Function ret add : (" + ret2 + ")");
+    }
+
 
 
 }
