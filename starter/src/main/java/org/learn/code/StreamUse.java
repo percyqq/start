9786fa45f295fdb21a378009868a21ec197d0346
qingqing005 <qingqing005@BKJK.com>
2021-12-03 18:25:04 +0800

    StreamUse

--- starter/src/main/java/org/learn/code/StreamUse.java
+++ starter/src/main/java/org/learn/code/StreamUse.java
@@ -3,9 +3,7 @@ package org.learn.code;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -33,12 +31,17 @@ public class StreamUse {
         l.add(new U(2, "3", "6"));
         l.add(new U(2, "3", "6"));
         l.add(new U(2, "4", "61"));
-        l.add(new U(3,      "4", "62"));
+        l.add(new U(3, "4", "62"));
         l.add(new U(3, "3", "6"));
 
         Map<Integer, List<String>> mmp = l.stream().collect(
                 Collectors.groupingBy(U::getId, Collectors.mapping(U::getName, Collectors.toList()))
         );
+
+        // 打平取出所有U，返回List
+        Map<String, List<U>> nmnp = new HashMap<>();
+        Collection<List<U>> cu = nmnp.values();
+        List<U> w666 = cu.stream().flatMap(Collection::stream).collect(Collectors.toList());
 
         System.out.println(mmp);
     }
