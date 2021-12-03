acbb4844d84c341c9b6f41659d2fcf873cba212f
qingqing005 <qingqing005@BKJK.com>
2021-12-03 18:39:05 +0800

    StreamUse

--- starter/src/main/java/org/learn/code/StreamUse.java
+++ starter/src/main/java/org/learn/code/StreamUse.java
@@ -2,8 +2,10 @@ package org.learn.code;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -38,12 +40,34 @@ public class StreamUse {
                 Collectors.groupingBy(U::getId, Collectors.mapping(U::getName, Collectors.toList()))
         );
 
+        //分组同时还转换
+        Function<U, U1> function = u -> {
+            U1 u1 = new U1();
+            BeanUtils.copyProperties(u, u1);
+            return u1;
+        };
+
+        List<U> users = l;
+        Map<Integer, List<U1>> userMap = users.stream().collect(
+                Collectors.groupingBy(U::getId, Collectors.mapping(function, Collectors.toList()))
+        );
+        System.out.println(" userMap ==> " + userMap);
+
+
         // 打平取出所有U，返回List
-        Map<String, List<U>> nmnp = new HashMap<>();
-        Collection<List<U>> cu = nmnp.values();
-        List<U> w666 = cu.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        Collection<List<U1>> cu = userMap.values();
+        List<U1> w666 = cu.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        System.out.println(" w666 ==> " + w666);
 
         System.out.println(mmp);
+    }
+
+
+    @Data
+    static class U1 {
+        int id;
+        String name;
+        String desc;
     }
 
 
